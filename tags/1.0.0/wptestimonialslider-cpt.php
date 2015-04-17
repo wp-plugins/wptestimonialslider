0e10be85156a4ad77356ce7bab5136557f3db178
wptestimonialslider <wptestimonialslider@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-17 17:50:38 +0000

    initial release
    git-svn-id: https://plugins.svn.wordpress.org/wptestimonialslider@1137402 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0.0/wptestimonialslider-cpt.php
@@ -0,0 +1,49 @@
+<?php 
+
+/**
+* Registers a new post type
+* @uses $wp_post_types Inserts new post type object into the list
+*
+* @param string  Post type key, must not exceed 20 characters
+* @param array|string  See optional args description above.
+* @return object|WP_Error the registered post type object, or an error object
+*/
+function wpt_Slider() {
+
+	$labels = array(
+		'name'               => _x( 'Testimonials', 'wptestimonialslider' ),
+		'singular_name'      => _x( 'Testimonial', 'wptestimonialslider' ),
+		'menu_name'          => _x( 'Testimonials', 'admin menu', 'wptestimonialslider' ),
+		'name_admin_bar'     => _x( 'Testimonial', 'add new on admin bar', 'wptestimonialslider' ),
+		'add_new'            => _x( 'Add New', 'testimonial', 'wptestimonialslider' ),
+		'add_new_item'       => __( 'Add New Testimonial', 'wptestimonialslider' ),
+		'new_item'           => __( 'New Testimonial', 'wptestimonialslider' ),
+		'edit_item'          => __( 'Edit Testimonial', 'wptestimonialslider' ),
+		'view_item'          => __( 'View Testimonial', 'wptestimonialslider' ),
+		'all_items'          => __( 'All Testimonials', 'wptestimonialslider' ),
+		'search_items'       => __( 'Search Testimonials', 'wptestimonialslider' ),
+		'parent_item_colon'  => __( 'Parent Testimonials:', 'wptestimonialslider' ),
+		'not_found'          => __( 'No testimonials found.', 'wptestimonialslider' ),
+		'not_found_in_trash' => __( 'No testimonials found in Trash.', 'wptestimonialslider' ),
+	);
+
+	$args = array(
+			'labels'             => $labels,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'wptestimonialslider' ),
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => false,
+			'menu_position'      => 5,
+			'menu_icon'          => 'dashicons-editor-quote',
+			'supports'           => array( 'title', 'editor','thumbnail')
+		);
+
+		register_post_type( 'wptestimonialslider', $args );
+}
+
+add_action( 'init', 'wpt_Slider' );
---
+++ tags/1.0.0/wptestimonialslider-shortcode.php
@@ -0,0 +1,101 @@
+<?php 
+
+
+function wpt_slider_trigger(){
+?>
+
+<script type="text/javascript">
+jQuery(document).ready(function(){
+	jQuery( ".cycle-slideshow, .cycle-nav" ).on( "mouseenter", function() {
+		jQuery(".cycle-nav").css("display", "block");
+	}).on( "mouseleave", function() {
+		jQuery(".cycle-nav").css("display", "none");
+	});
+});
+</script>
+
+<?php
+}
+add_action('wp_footer','wpt_slider_trigger');
+
+
+
+add_shortcode( 'wpt_testimonial', 'wpt_shortcode' );
+
+function wpt_shortcode() {
+
+	$loop = new WP_Query(
+		array(
+			'post_type'	=> 'wptestimonialslider',
+			'order_by'	=> 'title',
+			'title'		=> 'yes'
+			)
+		);
+
+	$output = '<div class="gs_testimonial_container">';
+		if ( $loop->have_posts() ) {
+
+			$output .= '<div class="cycle-slideshow composite-example" 
+						data-cycle-fx="carousel"
+						data-cycle-carousel-fluid="true"
+						data-cycle-center-horz="true"
+    					data-cycle-center-vert="true"
+						data-cycle-carousel-visible="1"
+						data-cycle-slides="> div"
+						data-cycle-next="#next"
+						data-cycle-prev="#prev"
+						data-cycle-pager="#custom-pager"
+						data-cycle-pager-template="<a>{{slideNum}}</a>"
+						data-cycle-timeout="3000">';
+
+			while ( $loop->have_posts() ) {
+				$loop->the_post();
+				$meta = get_post_meta( get_the_id() );
+
+				$wpt_id = get_post_thumbnail_id();
+				$gs_testimonial_url = wp_get_attachment_image_src($wpt_id, array(86,86),true);
+
+				$gs_testimonial = $gs_testimonial_url[0];
+				$gs_testimonial_alt = get_post_meta($wpt_id,'_wp_attachment_image_alt',true);
+
+
+
+				$output .= '<div class="gs_testimonial_single">';
+				$output .= '<div class="testimonial-box">';
+				$output .= '<div class="box-content"><p>'. get_the_content() .'</p></div>';
+				$output .= '<h3 class="box-title">'. get_the_title() .'</h3>';
+				$output .= '<div class="box-image"><img src="'. $gs_testimonial .'" alt="'. $gs_testimonial_alt .'"></div>';
+
+				if($meta['gs_t_client_company'][0]){
+
+					$output .= '<div class="box-companyname"><span>Company Name:</span> '. $meta['gs_t_client_company'][0] .'</div>';
+
+				}
+
+				if($meta['gs_t_client_design'][0]){
+
+					$output .= '<div class="box-designation"><span>Designation:</span> '. $meta['gs_t_client_design'][0] .'</div>';
+
+				}
+
+				$output .= '</div></div>';
+
+			}
+
+			$output .= '</div>';
+
+			$output .= '<div class="center cycle-nav"><a id="prev">Prev</a><a id="next">Next</a></div>';
+
+			$output .= '<div class="cycle-pager" id="custom-pager"></div>';
+
+		} else {
+			$output .= "No Testimonial Added!";
+		}
+
+	$output .= '</div>';
+
+	return $output;
+}
+
+
+
